fcd553c191a207ec3e573d15d1f0e5677ba7a6c4
Minh Nguyen <minh3d@gmail.com>
2016-02-17 09:17:45 +0700

    update

--- Assets/script/Effect/Ef_WebBoom.cs
+++ Assets/script/Effect/Ef_WebBoom.cs
@@ -1,12 +1,6 @@
 using UnityEngine;
 using System.Collections;
 
-/// <summary>
-/// 网效果
-/// </summary>
-/// <remarks>
-/// 效果总时间等于TimeWebScaleUp + TimeShake
-/// </remarks>
 public class Ef_WebBoom : MonoBehaviour {
     [System.NonSerialized]
     public GameObject Prefab_GoSpriteWeb; //不有editor赋值,由Ef_WebGenerate赋值
@@ -29,10 +23,6 @@ public class Ef_WebBoom : MonoBehaviour {
     tk2dSprite mSprWeb;
     private static Vector3[] mShakeDirects;
 
-
-    //时间线
-    //|——放大——|——震动——|
-    //                |—渐隐—|
 
     enum State1
     {
@@ -103,16 +93,12 @@ public class Ef_WebBoom : MonoBehaviour {
                     }
                     else
                     {
-                        //float elapse = 0F;
-                        //int curDirectIdx = 0;
 
                         mTsWeb.localPosition += (mShakeDirects[mIdxCurShakePos % mShakeDirects.Length] * RangeShake);
                         ++mIdxCurShakePos;
 
                         mElapseShakeInterval = 0;
-                        //elapse += IntervalShake;
                     }
-                    //yield return new WaitForSeconds(IntervalShake);
             }
             else
             {
@@ -138,88 +124,7 @@ public class Ef_WebBoom : MonoBehaviour {
         {
             mColorWeb.a = 1F - mElapseFadeout / TimeFadeOut;
             mSprWeb.color = mColorWeb;
-          
-            //c.a = 0F;
-            //mSprWeb.color = c;
-
             mElapseFadeout += Time.deltaTime;
         }
     }
-    //// Use this for initialization
-    //IEnumerator Start () { 
-    //    tk2dSprite sprWeb = Pool_GameObj.GetObj(Prefab_GoSpriteWeb).GetComponent<tk2dSprite>();
-    //    if (NameSprite != null && NameSprite != "")
-    //        sprWeb.spriteId = sprWeb.GetSpriteIdByName(NameSprite);
-    //    Transform ts = sprWeb.transform;
-    //    sprWeb.gameObject.SetActive(true);
-    //    ts.parent = transform;
-    //    ts.localPosition = Vector3.zero;
-    //    Color c = ColorInitialize;
-    //    c.a = 1F;
-    //    sprWeb.color = c;
-    //    mTsWeb = ts;
-    //    float elapse = 0F;
-    //    //放大
-    //    while (elapse < TimeWebScaleUp)
-    //    {
-    //        mTsWeb.localScale = (ScaleTarget * elapse / TimeWebScaleUp) * Vector3.one;
-    //        elapse += Time.deltaTime;
-    //        yield return 0;
-    //    }
-    //    mTsWeb.localScale = Vector3.one * ScaleTarget;  
- 
-    //    //震动
-    //    StartCoroutine(_Coro_Shake());
-    //    yield return new WaitForSeconds(TimeShake - TimeFadeOut);
-    //    //渐隐
-    //    elapse = 0F; 
-
-    //    while (elapse < TimeFadeOut)
-    //    {
-    //        c.a = 1F - elapse/TimeFadeOut;
-    //        sprWeb.color = c;
-    //        elapse += Time.deltaTime;
-    //        yield return 0;
-    //    }
-    //    c.a = 0F;
-    //    sprWeb.color = c;
-    //    yield return new WaitForSeconds(0.5F);
-
-    //    sprWeb.gameObject.SetActive(false);
-    //    Pool_GameObj.RecycleGO(Prefab_GoSpriteWeb, sprWeb.gameObject);
-    //    Destroy(gameObject);
-    //}
-     
-
-    //IEnumerator _Coro_Shake()
-    //{
-    //    if (mShakeDirects == null)
-    //    {
-    //        mShakeDirects = new Vector3[]{
-    //            new Vector3(-1F,1F,0F)
-    //            ,new Vector3(1F,-1.2F,0F)
-    //            ,new Vector3(1.2F,1F,0F)
-    //            ,new Vector3(-1.2F,-0.8F,0F)
-    //        };
-    //    }
-    //    float elapse = 0F;
-    //    int curDirectIdx = 0;
-    //    while (elapse < TimeShake)
-    //    {
-    //        mTsWeb.localPosition += (mShakeDirects[curDirectIdx%mShakeDirects.Length] * RangeShake);
-    //        ++curDirectIdx;
-    //        elapse += IntervalShake;
-    //        yield return new WaitForSeconds(IntervalShake);
-    //    }
-
-    //}
-
-    
-    //void OnGUI()
-    //{
-    //    if (GUILayout.Button("boom"))
-    //    {
-    //        StartCoroutine(Start1());
-    //    }
-    //}
 }
